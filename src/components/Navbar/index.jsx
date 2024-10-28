253a97904a256da32036ee0ec953683ac402e4f8
dicky ardiar <dickyardiar1@gmail.com>
2024-10-28 11:04:06 +0700

    adding and filter env secret

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -46,7 +46,6 @@ const NavbarSticky = () => {
               </Button>
             </div>
 
-            {/* Mobile Menu Button */}
             <IconButton
               variant="text"
               className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
